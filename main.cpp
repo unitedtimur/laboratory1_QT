42ae7465fcccc2f865ea158152e678dd121aaeaa
Timur Likhomanov <lihomanov.t@gmail.com>
2020-02-23 03:03:29 +0700

    deleted tests

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 #include "Checkfile.h"
 #include "Configuration.h"
 
-#include <iostream>
-#include <windows.h>
-#include <conio.h>
-#include <dos.h>
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -19,18 +14,6 @@ int main(int argc, char *argv[])
 
     checkFile.startTerminalThread();
     checkFile.startCheckPropertiesThread();
-
-    //HANDLE  hConsole;
-    //  int k;
-    //
-    //hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    //// you can loop k higher to see more color choices
-    //for(k = 1; k < 255; k++)
-    //{
-    //  // pick the colorattribute k you want
-    //  SetConsoleTextAttribute(hConsole, k);
-    //  QTextStream(stdout) << k << " I want to be nice today!" << endl;
-    //}
 
     return a.exec();
 }
